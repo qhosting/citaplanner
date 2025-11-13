4f4b66a67a215cd12c3cbb95671f67b950b5c319
CitaPlanner Deploy <deploy@citaplanner.com>
2025-11-13 03:39:38 +0000

    chore: force rebuild - update BUILD_DATE to 2025-11-13T03:39:37Z

--- app/lib/version.ts
+++ app/lib/version.ts
@@ -1,4 +1,4 @@
 // Auto-generated file - Do not edit manually
 export const APP_VERSION = '1.11.1';
-export const BUILD_DATE = '2025-11-13T03:36:42Z';
+export const BUILD_DATE = '2025-11-13T03:39:37Z';
 export const GIT_HASH = 'e667173';
